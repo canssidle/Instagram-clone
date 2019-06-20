efe55d0263fcd210cf62dc4eee1c5f2765f235df
canssidle <canssidlewairimu@gmail.com>
2019-06-20 12:31:03 +0300

    creat a photo class

--- insta/models.py
+++ insta/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
+from django.db import models
 
 
-# class Photo(models.Model):
-#     title  = models.CharField(max_length = 100)
-#     comment = models.CharField(max_length = 300)
-#     photo = models.ImageField(upload_to='photos/',default='DEFAULT VALUE')
-#     caption = models.CharField(max_length = 300)
+class Photo(models.Model):
+    title  = models.CharField(max_length = 100)
+    comment = models.CharField(max_length = 300)
+    photo = models.ImageField(upload_to='photos/',default='DEFAULT VALUE')
+    caption = models.CharField(max_length = 300)
     
 
 
