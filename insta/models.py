839b8709e1c5c34a60bc786266202f1a8b0a9a7c
canssidle <canssidlewairimu@gmail.com>
2019-06-20 12:30:13 +0300

    create the insta app

---
+++ insta/models.py
@@ -0,0 +1,14 @@
+# from django.db import models
+
+
+# class Photo(models.Model):
+#     title  = models.CharField(max_length = 100)
+#     comment = models.CharField(max_length = 300)
+#     photo = models.ImageField(upload_to='photos/',default='DEFAULT VALUE')
+#     caption = models.CharField(max_length = 300)
+    
+
+
+
+
+    
