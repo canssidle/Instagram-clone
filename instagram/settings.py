06e060c1777ce6811e41845c5cbac582914c53cf
canssidle <canssidlewairimu@gmail.com>
2019-06-25 12:14:22 +0300

    error

--- instagram/settings.py
+++ instagram/settings.py
@@ -100,18 +100,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'insta',
-#         'USER': 'canssidle',
-#         'PASSWORD':'judycharles',
-#     }
-          
-}
 
 
 # Password validation
